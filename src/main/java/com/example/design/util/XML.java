dfca44c87354f1bdb4c9f172bcf82ddc9a7caff5
admin <794475725@qq.com>
2023-05-30 14:07:21 +0800

    XML

--- src/main/java/com/example/design/util/XML.java
+++ src/main/java/com/example/design/util/XML.java
@@ -1,27 +1,33 @@
 package com.example.design.util;
 
 import com.qimen.api.QimenRequest;
+import com.qimen.api.response.SingleitemSynchronizeResponse;
 import com.taobao.api.internal.parser.xml.ObjectXmlParser;
 import com.taobao.api.internal.util.XmlWriter;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class XML {
 
-    private Logger log = LoggerFactory.getLogger(XML.class);
+    private static Logger log = LoggerFactory.getLogger(XML.class);
 
     /**
      * 奇门类转换xml，sdk自带的，也可以自己写
      * @param ob
      * @return
      */
-    public String parseQiMen(Object ob){
+    public static String parseQiMen(Object ob){
+        String title = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
         String write="";
-        XmlWriter tRsp= new XmlWriter(true, "request", QimenRequest.class);
+        XmlWriter tRsp= new XmlWriter(true, "response", QimenRequest.class);
         write = tRsp.write(ob);
-        return write;
+        return title+write;
     }
 
     /**
@@ -31,7 +37,7 @@ public class XML {
      * @param <T>
      * @return
      */
-    public <T>T convertQiMenClass(String body,Class<T> t)  {
+    public static <T>T convertQiMenClass(String body,Class<T> t)  {
         try {
             ObjectXmlParser xmlParser = new ObjectXmlParser(t);
             Object classObject = xmlParser.parse(body, "qimen1");
@@ -41,5 +47,16 @@ public class XML {
             log.info("转换奇门数据异常",e);
         }
         return null;
+    }
+
+    public static void main(String[] args) {
+        Date date = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String dateString = format.format(date);
+        SingleitemSynchronizeResponse response = new SingleitemSynchronizeResponse();
+        response.setItemId("122121");
+        response.setFlag("success");
+        String s = parseQiMen(response);
+        System.out.println(s);
     }
 }
