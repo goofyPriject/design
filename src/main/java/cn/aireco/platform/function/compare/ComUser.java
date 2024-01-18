1702c753449b5e7bc30258db61d104338c4dfd73
liugaofei <liugaofei1006@aliyun.com>
2024-01-18 16:36:25 +0800

    排序

---
+++ src/main/java/cn/aireco/platform/function/compare/ComUser.java
@@ -0,0 +1,14 @@
+package cn.aireco.platform.function.compare;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ComUser {
+
+    private Long id;
+
+    private Integer age;
+
+}
